0db0bbc098dd97fef5397c360779e2fbe15b753b
Alex <urus.urusik1337@gmail.com>
2023-03-27 18:01:39 +0700

    Реализовано приложение, структура приведена к принципам SOLID, интерфейс выполнен на платформе WPF

---
+++ Entropy/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Entropy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public EntropyCalculation calculates;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculates = new EntropyCalculation();
+        }
+
+        private void Message_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            calculates.Message = Message.Text;
+            calculates.CalcAllFields();
+            PrintFields();
+        }
+
+        private void PrintFields()
+        {
+            Ansambl.Text = calculates.Ansambl.ToString();
+            EntropyValue.Text = calculates.Entropy.ToString();
+            MaxEntropy.Text = calculates.MaxEntropy.ToString();
+            UnderLoadAlphabet.Text = calculates.UnderLoadAlphabet.ToString();
+            EntropyFirstStage.Text = calculates.EntropyFirstStage.ToString();
+            TwoSymbolGrid.ItemsSource = calculates.BigramProbabilities;
+            OneSymbolGrid.ItemsSource = calculates.SymbolProbabilities;
+        }
+    }
+
+    
+}
